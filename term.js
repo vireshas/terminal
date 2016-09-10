67d10f6762ad5ed831bdc11e40070736ce296bcf
Viresh S <asviresh@gmail.com>
2016-09-10 16:08:34 +0530

    rm unwanted code

--- term.js
+++ term.js
@@ -2,32 +2,6 @@ var hterm = require('hterm-umdjs').hterm
 var lib = require('hterm-umdjs').lib
 var spawn = require('child_pty').spawn
 
-const zsh = /zsh: (?:(?:command not found)|(?:no such file or directory)): ((?:https?:\/\/)|(?:file:\/\/)|(?:\/\/))?([^\n]+)/;
-const domainRegex = /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b|^localhost$|^127(?:\.[0-9]+){0,2}\.[0-9]+$|^(?:0*:)*?:?0*1$/;
-
-function isUrlCommand(data) {
-  const matcher = zsh
-  if (undefined === matcher || !data) return null;
-
-  const match = data.match(matcher);
-  if (!match) return null;
-  const protocol = match[1];
-  const path = match[2];
-
-  if (path) {
-    if (protocol) {
-      return `${protocol}${path}`;
-    }
-    // extract the domain portion from the url
-    const domain = path.split('/')[0];
-    if (domainRegex.test(domain)) {
-      return `http://${path}`;
-    }
-  }
-
-  return null;
-}
-
 const { app, BrowserWindow, shell, Menu } = require('electron').remote;
 
 //let c= new BrowserWindow({modal: true, show: false})
@@ -53,7 +27,6 @@ t.onTerminalReady = function() {
   io.sendString = inputHandler;
 }
 
-
 function isImage(data) {
   if ( data.indexOf(".png") != -1) {
     return true
@@ -63,7 +36,6 @@ function isImage(data) {
 }
 
 function inputHandler(data) {
-  debugger
   if (isImage(data)) {
     console.log("Found url: " + data)
     img = `<br /><img src='${data}'> </img>`
